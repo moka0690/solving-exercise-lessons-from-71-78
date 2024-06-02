2f89b6da19cc371cffefee4d1c854853e0260153
moka0690 <madradz39@gmail.com>
2024-06-02 21:10:50 +0100

    Exercise solution  3

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ return isNaN(parseInt(ele))? ele :""
 let manualjoin =ignoreNumbers.reduce((acc,ele)=> acc + ele, "");
 console.log(manualjoin);
 
+
+
+
+let myString = "EElllzzzzzzzeroo";
+
+let array = myString.split('')
+let result = array.filter((char, index, arr) => char !== arr[index - 1]).join('');
+
+console.log(result);  // "Elzero"
+
+
+let myArray = ["E", "l", "z", ["e", "r"], "o"];
+
+let concatArray = myArray.reduce((acc, curr) => acc.concat(curr), []).join('');
+
+console.log(concatArray);
